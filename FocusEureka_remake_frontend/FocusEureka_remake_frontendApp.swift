faa7dc5354b074359511f0d4025b653b1c57aa87
yuzchen7 <yuzchen7@gmail.com>
2024-08-28 16:40:30 -0400

    Initial Commit

---
+++ FocusEureka_remake_frontend/FocusEureka_remake_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusEureka_remake_frontendApp.swift
+//  FocusEureka_remake_frontend
+//
+//  Created by yuz_chen on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusEureka_remake_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusEureka_remake_frontendUITests/FocusEureka_remake_frontendUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FocusEureka_remake_frontendUITestsLaunchTests.swift
+//  FocusEureka_remake_frontendUITests
+//
+//  Created by yuz_chen on 8/28/24.
+//
+
+import XCTest
+
+final class FocusEureka_remake_frontendUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
